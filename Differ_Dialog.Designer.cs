7e394b4edcb887da926c5291c04898e315edf2ba
aaron710 <aaron710@users.noreply.github.com>
2016-10-19 17:34:57 -0400

    Initial Commit
    Getting project on Github

---
+++ Differ_Dialog.Designer.cs
@@ -0,0 +1,172 @@
+﻿using System.Windows.Forms;
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_file2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.file1_button = new System.Windows.Forms.Button();
+            this.file2_button = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.diff_start_button = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File 1";
+            // 
+            // label_file2
+            // 
+            this.label_file2.AutoSize = true;
+            this.label_file2.Location = new System.Drawing.Point(9, 88);
+            this.label_file2.Name = "label_file2";
+            this.label_file2.Size = new System.Drawing.Size(32, 13);
+            this.label_file2.TabIndex = 1;
+            this.label_file2.Text = "File 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output Directory";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(277, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(98, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(277, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // file1_button
+            // 
+            this.file1_button.BackColor = System.Drawing.Color.Transparent;
+            this.file1_button.ForeColor = System.Drawing.Color.Black;
+            this.file1_button.Location = new System.Drawing.Point(404, 45);
+            this.file1_button.Name = "file1_button";
+            this.file1_button.Size = new System.Drawing.Size(75, 23);
+            this.file1_button.TabIndex = 6;
+            this.file1_button.Text = "Browse";
+            this.file1_button.UseVisualStyleBackColor = false;
+            this.file1_button.Click += new System.EventHandler(this.file1_Browse);
+            // 
+            // file2_button
+            // 
+            this.file2_button.Location = new System.Drawing.Point(404, 84);
+            this.file2_button.Name = "file2_button";
+            this.file2_button.Size = new System.Drawing.Size(75, 23);
+            this.file2_button.TabIndex = 7;
+            this.file2_button.Text = "Browse";
+            this.file2_button.UseVisualStyleBackColor = true;
+            this.file2_button.Click += new System.EventHandler(this.file2_Browse);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(404, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // diff_start_button
+            // 
+            this.diff_start_button.Location = new System.Drawing.Point(148, 176);
+            this.diff_start_button.Name = "diff_start_button";
+            this.diff_start_button.Size = new System.Drawing.Size(194, 23);
+            this.diff_start_button.TabIndex = 9;
+            this.diff_start_button.Text = "Start Diff";
+            this.diff_start_button.UseVisualStyleBackColor = true;
+            this.diff_start_button.Click += new System.EventHandler(this.StartDiff);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 218);
+            this.Controls.Add(this.diff_start_button);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.file2_button);
+            this.Controls.Add(this.file1_button);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label_file2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Differ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_file2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button file1_button;
+        private System.Windows.Forms.Button file2_button;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button diff_start_button;
+    }
+}
+
---
+++ Differ_Dialog.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        private int NUM_OF_LINES_DICTIONARY = 4000000;
+
+        private string m_strFileToCompare;
+        private string m_strFileToCompareAgainst;
+        private string m_strDirectoryOutputTo;
+        private OpenFileDialog openFileDialog;
+        private FolderBrowserDialog folderBrowserDialog1;
+
+        StreamWriter logWriter;
+        StreamWriter diffLeftWriter;
+        StreamWriter diffRightWriter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*  Name: file1_Browse()
+         *  Desc: Generates an open file dialog box and waits for user selection.
+         *        once the file is selected it sets it to the text box's variable
+         *        in the UI and sets it to fileToCompare
+         *  
+         * 
+         */
+        private void file1_Browse(object sender, EventArgs e)
+        {
+            int num = -1;
+
+            DialogResult result = this.openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.m_strFileToCompare = openFileDialog.FileName;
+                try
+                {
+                    this.textBox1.Text = this.m_strFileToCompare;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+
+        private void file2_Browse(object sender, EventArgs e)
+        {
+            DialogResult result = this.openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileName = this.openFileDialog.FileName;
+                this.m_strFileToCompareAgainst = fileName;
+                try
+                {
+                    this.textBox2.Text = fileName;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.m_strDirectoryOutputTo = this.folderBrowserDialog1.SelectedPath;
+                try
+                {
+                    this.textBox3.Text = this.m_strDirectoryOutputTo;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void StartDiff(object sender, EventArgs e)
+        {
+            string str;
+           
+            int num = 0;
+
+            // Create a dictionary for our initial data set ~ 4000000 lines of a file
+            // NOTE: This may need to be tweaked based on text file type. But for our
+            //       initial usage of this application it works.
+            Dictionary<int, Dictionary<string, int>> allDictionaries = new Dictionary<int, Dictionary<string, int>>();
+
+            // Create our initial dictionary for Line Conetent and Line number
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            // Assign the loggers to their own files
+            logWriter = new StreamWriter(this.m_strDirectoryOutputTo + "\\DifferLog.txt");
+            diffLeftWriter = new StreamWriter(this.m_strDirectoryOutputTo + "\\DiffLeft.txt");
+            diffRightWriter = new StreamWriter(this.m_strDirectoryOutputTo + "\\DiffRight.txt");
+
+            // Here is where we do the file 1 to file 2 comparrison
+            doDictionaryAdd(ref dictionary, ref allDictionaries, this.m_strFileToCompareAgainst);
+            diffLeftWriter.WriteLine("Lines in {0} not in {1}\n", m_strFileToCompare, m_strFileToCompareAgainst);
+            diffLeftWriter.WriteLine("\n");
+            doCompare(ref dictionary, ref allDictionaries, ref diffLeftWriter, this.m_strFileToCompare);
+          
+            // We need to reinitialize our dictionary here, as well as clear our dictionary of dictionaries
+            dictionary = new Dictionary<string, int>();
+            allDictionaries.Clear();
+
+
+            // Here is where we do the file 2 to file 1 comparrison
+            doDictionaryAdd(ref dictionary, ref allDictionaries, this.m_strFileToCompare);
+            diffRightWriter.WriteLine("Lines in {0} not in {1}\n", m_strFileToCompareAgainst, m_strFileToCompare);
+            diffRightWriter.WriteLine("\n");
+            doCompare(ref dictionary, ref allDictionaries, ref diffRightWriter, this.m_strFileToCompareAgainst);
+
+            // Clear the dictionary one last time
+            dictionary.Clear();
+            allDictionaries.Clear();
+             
+            MessageBox.Show("Diff Has Ended");
+
+            closeResources(logWriter, diffLeftWriter, diffRightWriter);
+        }
+
+        private void doDictionaryAdd(ref Dictionary<String,int> dictionary, ref Dictionary<int,Dictionary<String,int>> allDictionaries, String readFilePath)
+        {
+            StreamReader reader = new StreamReader(readFilePath);
+
+            int num = 0;
+            int dictionaryKey = 0;
+            String str = "";
+
+            while ((str = reader.ReadLine()) != null)
+            {
+                num++;
+
+                try
+                {
+                    // If the line number divided our number of dictionaries is equal to our
+                    // constant for number of lines before we clear our dictionary and start again 
+                    if ((num / (dictionaryKey + 1)) == NUM_OF_LINES_DICTIONARY)
+                    {
+                        // Addd our current dictionary to the list of all dictionaries
+                        allDictionaries.Add(dictionaryKey, dictionary);
+
+                        // Assign our single use dictionary
+                        dictionary = new Dictionary<string, int>();
+                        dictionaryKey++;
+
+                        // Log that we have gone over our limit of the constant and continue
+                        logWriter.WriteLine("New Dictionary Created due to current line number being a multiple of {0}", NUM_OF_LINES_DICTIONARY);
+
+                    }
+
+                    dictionary.Add(str, 1);
+
+                }
+                catch (Exception ex)
+                {
+                    logWriter.WriteLine("Failed to add line {0}, string {1} (Exception Occurred: HelpLink = {2} , Message = {3}, Source = {4}, StackTrace = {5}, TargetSite = {6})", num, str, ex.HelpLink, ex.Message, ex.Source, ex.StackTrace, ex.TargetSite);
+                }
+
+            }
+
+            // We need to add our final dictionary to our list of dictionaries since we didnt
+            // add it within the loop
+            allDictionaries.Add(dictionaryKey, dictionary);
+
+            reader.Close();
+        }
+
+        private void doCompare(ref Dictionary<String, int> dictionary, ref Dictionary<int, Dictionary<String, int>> allDictionaries, ref StreamWriter writer, String fileToRead)
+        {
+            StreamReader reader2 = new StreamReader(fileToRead);
+            int num = 0;
+            String str = "";
+
+            while ((str = reader2.ReadLine()) != null)
+            {
+                num++;
+
+                var coll = allDictionaries.Values;
+                bool tFound = false;
+                foreach (var dic in coll)
+                {
+                    // if the dictionary does not contain the key it is not found we want to
+                    // output this to our file for found in 1 but not the other with the line number
+                    if (dic.ContainsKey(str))
+                    {
+                        tFound = true;
+                        break;
+                    }
+                }
+
+                if (tFound == false)
+                {
+                    string strOut;
+                    strOut = str + " , " + num;
+                    writer.WriteLine(strOut);
+                }
+            }
+
+            reader2.Close();
+
+            // Add some lines for some spacing
+            writer.WriteLine("\n");
+            writer.WriteLine("\n");
+
+        }
+
+        private void closeResources(StreamWriter writer1, StreamWriter writer2, StreamWriter writer3)
+        {
+            writer1.Close();
+            writer2.Close();
+            writer3.Close();
+        }
+
+    }// End of Form1
+
+}
